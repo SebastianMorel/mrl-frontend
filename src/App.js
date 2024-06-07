0fc54121ff00f018805a3d6535ca530bba5080f4
mrl <sebastian@mrl.dev>
2024-06-08 01:03:30 +0200

    -

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function App() {
   const [showSocial, setShowSocial] = useState(false);
   const [showProjects, setShowProjects] = useState(false);
   const [projectStatuses, setProjectStatuses] = useState({});
-  const backendUrl = process.env.BACKEND_URL;
+  const backendUrl = 'https://mrl-backend-m8hi4tz67-sebastianmorels-projects.vercel.app';
 
   async function checkStatus(domain) {
     try {
