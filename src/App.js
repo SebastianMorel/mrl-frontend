03443f502ccf3592b542658b7494863a60ba3435
Sebastian Morel <morrisxdd@pm.me>
2025-04-20 15:52:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -115,7 +115,8 @@ const projectLogos = {
   "plotandpray.com": ["flask","python","chatgpt"],
   "google.com": ["flask","python","geojson"],
   "sanity.com": ["sanity", "powerbi", "vercel"],
-  "blog-two-alpha-77.vercel.app": ["sanity", "powerbi", "vercel"]
+  "blog-two-alpha-77.vercel.app": ["sanity", "powerbi", "vercel"],
+  "swestad-kqb06emr6-morrisprojects.vercel.app": ["flask", "python", "geojson"]
 };
 
 function App() {
@@ -254,6 +255,11 @@ useEffect(() => {
             </a> soundtrack tier list
             <br></br>
             {renderLogosForProject("google.com")}
+            <a href="https://swestad.mrl.dev/" target="_blank" rel="noopener noreferrer">
+              swestad.<span className="normal-color">mrl.dev</span>
+            </a> company city tierlist
+            <br></br>
+            {renderLogosForProject("swestad-kqb06emr6-morrisprojects.vercel.app")}
             <a href="https://swehome.mrl.dev/" target="_blank" rel="noopener noreferrer">
               swehome.<span className="normal-color">mrl.dev</span>
             </a> swedish housing data
